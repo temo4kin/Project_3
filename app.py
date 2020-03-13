0d8a05a549377493066f296736d7f5ce9a1ce7da
Artem <temo4kinn@gmail.com>
2020-03-13 21:25:09 +0300

    Исправление ошибок прошлого проекта

--- app.py
+++ app.py
@@ -2,14 +2,18 @@ import json
 import datetime
 from random import sample
 from flask import Flask, render_template, request
-from data import *
+from data import goals, weekdays, teachers
+
+teachers_random = sample(teachers, 6)
+year = datetime.datetime.now().year
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template(
-        "index.html",
+        'index.html',
         goals=goals,
         teachers_random=teachers_random,
         teachers=teachers,
@@ -18,7 +22,7 @@ def index():
 
 @app.route('/all')
 def all():
-    return render_template("all.html", goals=goals, teachers=teachers, year=year)
+    return render_template('all.html', goals=goals, teachers=teachers, year=year)
 
 @app.route('/goal/<goal_name>/')
 def goal(goal_name):
@@ -27,7 +31,7 @@ def goal(goal_name):
         if goal_name in teacher['goals']:
             teachers_goal.append(teacher)
     return render_template(
-        "goal.html",
+        'goal.html',
         goals=goals,
         teachers_goal=teachers_goal,
         goal_name=goal_name,
@@ -39,17 +43,17 @@ def goal(goal_name):
 def profile(uin):
     work_week = {}
     days_week = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+
     num = 0
-    for i in range(1):
-        for day in teachers[uin]['free']:
-            days_new = {x: True for x in teachers[uin]['free'][day].keys() if teachers[uin]['free'][day][x] == True}
-            work_week[days_week[num]] = days_new
-            num += 1
+    for day in teachers[uin]['free']:
+        free_slots = {x: True for x in teachers[uin]['free'][day].keys() if teachers[uin]['free'][day][x] == True}
+        work_week[days_week[num]] = free_slots
+        num += 1
+
     return render_template(
-        "profile.html",
+        'profile.html',
         teachers=teachers,
         uin=uin,
-        teachers_number=[1],
         goals=goals,
         weekdays=weekdays,
         work_week=work_week,
@@ -59,7 +63,7 @@ def profile(uin):
 
 @app.route('/request')
 def request_client():
-    return render_template("request.html", year=year)
+    return render_template('request.html', year=year)
 
 
 @app.route('/request_done', methods=['POST'])
@@ -72,10 +76,10 @@ def request_done():
                           time=time,
                           name=client_name_request,
                           phone=client_phone_request)
-    with open("request.json", "a") as f:
+    with open('request.json', 'a') as f:
         json.dump(request_client, f)
     return render_template(
-        "request_done.html",
+        'request_done.html',
         goals_teachers=goals,
         goal=goal,
         time=time,
@@ -88,7 +92,7 @@ def request_done():
 @app.route('/booking/<int:teacher_id>/<day>/<time>')
 def booking(teacher_id, day, time):
     return render_template(
-        "booking.html",
+        'booking.html',
         teachers=teachers,
         teacher_id=teacher_id,
         weekdays=weekdays,
@@ -100,33 +104,30 @@ def booking(teacher_id, day, time):
 
 @app.route('/booking_done', methods=['POST'])
 def booking_done():
-    client_weekday = request.form.get("clientWeekday")
-    client_time = request.form.get("clientTime")
-    client_teacher = request.form.get("clientTeacher")
-    client_name = request.form.get("clientName")
-    client_phone = request.form.get("clientPhone")
+    client_weekday = request.form.get('clientWeekday')
+    client_time = request.form.get('clientTime')
+    client_teacher = request.form.get('clientTeacher')
+    client_name = request.form.get('clientName')
+    client_phone = request.form.get('clientPhone')
     client = dict(weekday=client_weekday,
                   time=client_time,
                   teacher=int(client_teacher),
                   name=client_name,
                   phone=client_phone)
-    with open("booking.json", "a") as f:
+    with open('booking.json', 'a') as f:
         json.dump(client, f)
+    print(client['weekday'], client['time'], client['name'])
     return render_template(
-        "booking_done.html",
+        'booking_done.html',
         weekdays=weekdays,
-        client_time=client_time,
-        client_teacher=client_teacher,
-        client_weekday=client_weekday,
-        client_name=client_name,
-        client_phone=client_phone,
+        client=client,
         year=year
     )
 
 @app.errorhandler(500)
 def render_server_error(error):
     return render_template(
-        "500.html",
+        '500.html',
         goals=goals,
         year=year
     ), 500
@@ -135,15 +136,11 @@ def render_server_error(error):
 @app.errorhandler(404)
 def render_not_found(error):
     return render_template(
-        "404.html",
+        '404.html',
         goals=goals,
         year=year
     ), 404
 
-
-teachers_random = sample(teachers, 6)
-
-year = datetime.datetime.now().year
 
 if __name__ == '__main__':
     app.run()
